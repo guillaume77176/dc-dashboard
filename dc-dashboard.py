7c196cd1314affe448213bf4c8b9e9046794be95
guillaume176 <guillaumer.176@gmail.com>
2026-05-01 03:22:47 +0000

    ff

--- dc-dashboard.py
+++ dc-dashboard.py
@@ -168,18 +168,19 @@ df = get_data_viz(data_global, columns_to_plot)
 df["index_time"] = pd.to_datetime(df["index_time"])
 
 
+try:
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    mask = (df["index_time"] >= start_date) & (df["index_time"] <= end_date)
+    df = df.loc[mask]
 
-start_date = pd.to_datetime(start_date)
-end_date = pd.to_datetime(end_date)
-mask = (df["index_time"] >= start_date) & (df["index_time"] <= end_date)
-df = df.loc[mask]
+    pred = func_c(data_it, model, end_date, steps=nb_step_predict)
 
-pred = func_c(data_it, model, end_date, steps=nb_step_predict)
+    actions_opt = get_control(data_control, end_date, steps=nb_step_predict)
 
-actions_opt = get_control(data_control, end_date, steps=nb_step_predict)
-
-df_interpret_10min = interpret(data_it, gam, end_date, steps=nb_step_predict)
-
+    df_interpret_10min = func_gam(data_it, interpret, end_date, steps=nb_step_predict)
+except NameError:
+    pass
 
 
 
